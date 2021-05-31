b9f8507f474763a1fc14fcffad40e448b432cda3
Majd Sabah <msabah@winfooz.com>
2021-05-31 13:09:51 +0300

    Initial Commit

---
+++ RicohTheta/RicohTheta.h
@@ -0,0 +1,18 @@
+//
+//  RicohTheta.h
+//  RicohTheta
+//
+//  Created by Majd Sabah on 31/05/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RicohTheta.
+FOUNDATION_EXPORT double RicohThetaVersionNumber;
+
+//! Project version string for RicohTheta.
+FOUNDATION_EXPORT const unsigned char RicohThetaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RicohTheta/PublicHeader.h>
+
+
